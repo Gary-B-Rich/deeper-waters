813cde8baa87f914123202c9ea50c94d89810b6c
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-03-05 13:50:16 -0500

    .js returning to top of page working

--- script.js
+++ script.js
@@ -18,4 +18,15 @@ button3.addEventListener("click", ()=> {
 const button4=document.getElementById("button4");
 button4.addEventListener("click", ()=> { 
     console.log("You clicked button 4");
+    scrollToTop();
 });
+
+// Smooth scroll to top of the screen function
+const scrollToTop = () => {
+    const scroller = document.documentElement.scrollTop || document.body.scrollTop;
+    if (scroller > 0) {
+      window.requestAnimationFrame(scrollToTop);
+      window.scrollTo(0, scroller - scroller / 30); //30 is the speed
+    }
+  };
+// use scrollToTop() to execute

271de5b5b85a54b9faeb43b9c645b9b267756f27
Gary-B-Rich <127971642+Gary-B-Rich@users.noreply.github.com>
2024-03-15 11:54:20 -0400

    tweaks and fixes

--- script.js
+++ script.js
@@ -2,50 +2,74 @@
 
 window.scrollTo(0,0);
 const text1=document.getElementById("text1");
+const text2=document.getElementById("text2");
+const text3=document.getElementById("text3");
+const text4=document.getElementById("text4");
 
 
 // church button
 const home=document.getElementById("photo");
 home.addEventListener("click", ()=> { 
-    console.log("You clicked the home button");
+    console.log("clicked photo button");
     scrollToTop();
     document.body.style.backgroundImage="linear-gradient(white, aliceblue, steelblue, black, black)";
-    text1.innerText="Welcome to Deeper Waters Christian Teaching Ministries";
+    updateChurch();
 });
 
 // "our mission" button
 const button1=document.getElementById("button1");
 button1.addEventListener("click", ()=> { 
-    console.log("You clicked button 1");
+    console.log("clicked button1");
     scrollToTop();
     document.body.style.backgroundImage="linear-gradient(steelblue, aliceblue, steelblue)";
-    text1.innerText=`Welcome to Deeper Waters`;
-    console.log(text1.innerText);
+    
 });
 
 // "worship services button"
 const button2=document.getElementById("button2");
 button2.addEventListener("click", ()=> { 
-    console.log("You clicked button 2");
+    console.log("clicked button2");
     scrollToTop();
     document.body.style.backgroundImage="linear-gradient(white, steelblue, white)";
+
 });
 
 // "contact our team button"
 const button3=document.getElementById("button3");
 button3.addEventListener("click", ()=> { 
-    console.log("You clicked button 3");
+    console.log("You clicked button3");
     scrollToTop();
-    //document.body.style.backgroundImage="linear-gradient(black, steelblue, black)";
-    document.body.style.backgroundImage="url(/deeper-waters/sunsetChurch.jpg)";
+    document.body.style.backgroundImage="linear-gradient(black, red, black)";
+    //document.body.style.backgroundImage="url(/deeper-waters/sunsetChurch.jpg)";
+
 });
 
 // "Jesus button"
 const button4=document.getElementById("button4");
 button4.addEventListener("click", ()=> { 
-    console.log("You clicked the Jesus button (back to top)");
+    console.log("clicked Jesus button4");
     scrollToTop();
 });
+
+// update dom - church button
+const updateChurch = () => {
+    text1.innerText="Lorem ipsum dolor sit amet consectetur adipisicing elit. \
+    Modi, libero! Aperiam officia tempore veniam aliquid expedita accusantium.\
+     Magni iste saepe maxime ex enim nostrum sequi ad totam explicabo \
+     consequuntur quia modi quidem maiores, rem esse minima, debitis libero \
+     ipsum cum asperiores? Quia culpa asperiores id, vel itaque vero assumenda\
+      incidunt modi veritatis aut dicta nemo obcaecati maiores, quod sint \
+      libero reiciendis voluptates ex? Cum tempora cumque doloribus \
+      accusantium error debitis aperiam maiores dolor repellendus deleniti.";
+    text2.innerText="Lorem ipsum dolor sit amet consectetur adipisicing elit. \
+    Modi, libero! Aperiam officia tempore veniam aliquid expedita accusantium.\
+     Magni iste saepe maxime ex enim nostrum sequi ad totam explicabo \
+     consequuntur quia modi quidem maiores, rem esse minima, debitis libero \
+     ipsum cum asperiores? Quia culpa asperiores id, vel itaque vero assumenda\
+      incidunt modi veritatis aut dicta nemo obcaecati maiores, quod sint \
+      libero reiciendis voluptates ex? Cum tempora cumque doloribus \
+      accusantium error debitis aperiam maiores dolor repellendus deleniti.";
+};
 
 // Smooth scroll to top of the screen function
 const scrollToTop = () => {
